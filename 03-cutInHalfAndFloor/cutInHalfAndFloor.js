cd6be6211f35f3d2ade33591a1077aa992da45ae
jonathan leibovici <jonathanleibovici@gmail.com>
2020-12-24 13:41:51 -0500

    finished 04

--- 03-cutInHalfAndFloor/cutInHalfAndFloor.js
+++ 03-cutInHalfAndFloor/cutInHalfAndFloor.js
@@ -1,7 +1,7 @@
 function cutInHalfAndFloor(num) {
   // Insert code here;
   
-  return Math.floor(num/2);
+  return Math.floor(num/2);// math.floor is a math function is javascript rounds down to the  int
 }
 
 // Do not edit this line;
--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,14 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  let count = 0;//we need a count 
+  for(let i =0 ;i<arr.length;i++){// loop the array 
+    if(arr[i]===true||arr[i]===false){//javascript has truthy and falsy values so we only need true and false 
+      //then return the count 
+      count++
+    }
+
+  }
+  return count
 }
 
 // Do not edit this line;
