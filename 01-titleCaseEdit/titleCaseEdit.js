4fe9ee0ff23adab2e13d4b0187f1a296a4990503
jonathan leibovici <jonathanleibovici@gmail.com>
2020-12-24 13:24:48 -0500

    finished 01 and 02

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,11 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  let str = title.split(" ");//split coverts it to an array 
+  for(let i = 0;i < str.length; i++){
+    str[i] = str[i][0].toUpperCase() + str[i].substr(1) //substr appends the rest of the elemt to the first letter that was capitlazed
+  }
+  let str1 = str.join(" ");//join covertes the array it into a string with the spaces 
+  return str1;
 }
 
 // Do not edit this line;
