b9abecab5560af4ab9a20c18cf3a1a5eaf40869e
jonathan leibovici <jonathanleibovici@gmail.com>
2020-12-25 14:09:03 -0500

    finished 07

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,17 @@
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
+  let count = 0 
+   
+ // let total = nums.length
+ for(let i = 0 ;i<nums.length;i++){
+   if(nums[i]===i)
+   {
+
+    count++
+   }
+
+ }return count
+ 
 }
 
 // Do not edit this line;
