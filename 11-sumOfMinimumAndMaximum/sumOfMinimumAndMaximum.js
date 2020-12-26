cb2cf79e74fd84c0238922ed45965c36d0cd173c
jonathan leibovici <jonathanleibovici@gmail.com>
2020-12-25 22:46:47 -0500

    finished 11

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,6 +1,14 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
-}
+  let sum = 0 
+    let lastItem = nums[nums.length - 1]
+    // to get the last item in the array just do nums is the array
+    //then the length of the array - 1 to get the last index
+    sum += nums[0] + lastItem
+  
+  
+  return sum
+ }
 
 // Do not edit this line;
 module.exports = sumOfMinimumAndMaximum;
