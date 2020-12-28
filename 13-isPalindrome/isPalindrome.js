a3235a56c893310cb9e2cc478ad07ce47627e244
jonathan leibovici <jonathanleibovici@gmail.com>
2020-12-27 22:59:12 -0500

    finished 14

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,9 +1,9 @@
 function isPalindrome(word) {
   // Insert code here;
   
-  let check = (word.length)/2;
+  let check = (word.length)/2;// create a variable and take the words length and divid by 2
   
-  for(let i = 0; i < check; i++){
+  for(let i = 0; i < check; i++){// loop to see if the word matches 
       if(word[i] != word[word.length-1-i]){
           return false;
       }
--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,16 @@
 function frequencyCounter(word) {
   // Insert code here;
+  var frequency = {};//an empty set
+    for (var i=0; i<word.length;i++) {//loop the word
+        var character = word.charAt(i);//returns a string 
+        if (frequency[character]) {//if that word chrt count it
+          frequency[character]++;
+        } else {//give it 1 count
+          frequency[character] = 1;
+        }
+    }
+
+    return frequency
 }
 
 // Do not edit this line;
