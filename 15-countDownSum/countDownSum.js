d43ec86f1eee7a431d583ecaeefa91e91c16db5a
jonathan leibovici <jonathanleibovici@gmail.com>
2020-12-30 15:38:07 -0500

    finished 15

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,9 +1,13 @@
 class MySolution {
   countDownSum(num) {
-    // Insert code here;
+    if(num == 1){//base case 
+      return 1;
+    }
+    else{
+      return num + this.countDownSum(num-1);//then the num adds the next number and keeps calling untill it hits one
+    }
   }
 }
-
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
