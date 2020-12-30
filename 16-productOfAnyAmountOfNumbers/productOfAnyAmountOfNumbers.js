94efc3fd03a823ac041bcf8863196f632adf6b58
jonathan leibovici <jonathanleibovici@gmail.com>
2020-12-30 15:47:50 -0500

    finished 16

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,10 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  let sum =1//you need the 1 
+  for(let i =0;i<arguments.length;i++)
+  {
+ sum *=arguments[i]
+} return sum
 }
 
 // Do not edit this line;
